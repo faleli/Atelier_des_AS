4932c6793c85aa1d34d3b12028c279d2466c6550
fuston <fuston.aleli@gmail.com>
2022-03-31 23:14:08 +0200

    mise en place du contenu de notre page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): response
+    public function index() 
     {
         return $this->render('home/index.html.twig');
     }
